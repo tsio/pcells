759042e03453e5f8d45d3e02ed49bb7db905393d
Christian Bernardt <christian.bernardt@desy.de>
2014-03-03 10:37:17 +0100

    Reformatting changes only

--- org/dcache/gui/pluggins/pnfs/PnfsManagerPanel.java
+++ org/dcache/gui/pluggins/pnfs/PnfsManagerPanel.java
@@ -2,214 +2,202 @@
 //
 package org.dcache.gui.pluggins.pnfs ;
 //
-import org.dcache.gui.pluggins.*;
-import org.dcache.gui.pluggins.pools.*;
+import org.pcells.services.connection.DomainConnection;
+import org.pcells.services.connection.DomainConnectionListener;
+import org.pcells.services.gui.CellGuiSkinHelper;
+
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.font.*;
-import javax.swing.*;
-import javax.swing.table.*;
-import javax.swing.event.* ;
-import javax.swing.border.* ;
-import java.util.*;
-import java.io.* ;
-import java.util.prefs.* ;
-import java.util.regex.*  ;
-import javax.swing.*;
-import org.pcells.services.connection.DomainConnection ;
-import org.pcells.services.connection.DomainConnectionListener ;
-import org.pcells.services.connection.DomainEventListener ;
-import org.pcells.services.gui.* ;
-import org.pcells.services.gui.util.* ;
-import diskCacheV111.vehicles.CostModulePoolInfoTable ;
-import diskCacheV111.pools.PoolCostInfo ;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.prefs.Preferences;
 
 
 public class      PnfsManagerPanel
-       extends    CellGuiSkinHelper.CellPanel 
-       implements ActionListener, DomainConnectionListener{
-                  
-   private DomainConnection _connection      = null ;
-   private Preferences      _preferences     = null ;
+        extends    CellGuiSkinHelper.CellPanel
+        implements ActionListener, DomainConnectionListener{
 
-   private boolean _debug = true ;
-   
-   private static final int REQ_PNFSMGR_INFO = 12123 ;
-   
-   private PnfsManagerInfoDisplay _counterDisplay = new PnfsManagerInfoDisplay();
-   private PnfsDetailPanel        _detailPanel    = new PnfsDetailPanel() ;
-  
-   private JPanel     _switchPanel  = null ;
-   private CardLayout _switchLayout = null ; 
+    private DomainConnection _connection      = null ;
+    private Preferences      _preferences     = null ;
+
+    private boolean _debug = true ;
+
+    private static final int REQ_PNFSMGR_INFO = 12123 ;
+
+    private PnfsManagerInfoDisplay _counterDisplay = new PnfsManagerInfoDisplay();
+    private PnfsDetailPanel        _detailPanel    = new PnfsDetailPanel() ;
+
+    private JPanel     _switchPanel  = null ;
+    private CardLayout _switchLayout = null ;
 
 
-   public PnfsManagerPanel( DomainConnection connection , Preferences preferences ){
-   
-      _connection  = connection ;
-      _preferences = preferences ;
-      
-      BorderLayout l = new BorderLayout(10,10) ;
-      setLayout(l) ;
-      
-      setBorder( BorderFactory.createTitledBorder( 
-	         BorderFactory.createLineBorder( Color.blue , 1 ) , 
+    public PnfsManagerPanel( DomainConnection connection , Preferences preferences ){
+
+        _connection  = connection ;
+        _preferences = preferences ;
+
+        BorderLayout l = new BorderLayout(10,10) ;
+        setLayout(l) ;
+
+        setBorder( BorderFactory.createTitledBorder(
+                BorderFactory.createLineBorder( Color.blue , 1 ) ,
                 "Pnfs Manager" ) ) ;
- 
-      add( new Controller( )  , "North" ) ;
-     
-      _switchPanel = new JPanel( _switchLayout = new CardLayout() ) ;
- 
 
-      JPanel inner = new JPanel( new GridBagLayout() ) ;
-      inner.add( _counterDisplay ) ;
+        add( new Controller( )  , "North" ) ;
 
-      _switchPanel.add(   inner , "main" ) ; 
-      _switchPanel.add(   _detailPanel    , "detail" ) ; 
+        _switchPanel = new JPanel( _switchLayout = new CardLayout() ) ;
 
-      add( _switchPanel , "Center" ) ;
-       
-   }
-   public void domainAnswerArrived( Object obj , int subid ){
-      
-      if( subid == REQ_PNFSMGR_INFO ){
-	 
-	    if( ( obj == null ) ){
-	       //
-	       // This is a timeout
-	       //
-	       //setWaiting(false);
-	       displayErrorMessage("Request to PnfsManager timed out" );
-	       
-	    }else if( obj instanceof dmg.cells.nucleus.NoRouteToCellException ){
-	       //
-	       // The SrmSpaceManager is not reachable.
-	       //
-	       setWaiting(false);
-	       displayErrorMessage("PnfsManager not present !" );
-	       	       
-	    }else if( obj instanceof String ){
-	       System.out.println(obj.toString());
-	       try{
 
-                  PnfsManagerInfo pnfsManagerInfo = PnfsManagerInfo.decodePnfsManagerInfo( (String)obj ) ;
+        JPanel inner = new JPanel( new GridBagLayout() ) ;
+        inner.add( _counterDisplay ) ;
 
-	          _counterDisplay.setPnfsManagerInfo( pnfsManagerInfo );
-                  _detailPanel.setMap( pnfsManagerInfo._statisticsMap ) ;
+        _switchPanel.add(   inner , "main" ) ;
+        _switchPanel.add(   _detailPanel    , "detail" ) ;
 
-	       }catch(PnfsManagerDecodingException pee ){
-		  pee.printStackTrace();
-	          setWaiting(false);
-	          displayErrorMessage("Problem in decoding message from PnfsManager at line "+pee.getLine() );
-	       }catch(Exception ee ){
-		  ee.printStackTrace();
-	          setWaiting(false);
-	          displayErrorMessage("Problem in decoding message from PnfsManager" );
-	       }
-	    }
-      }
-   }
-   private void askForPnfsInfo(){
-       try{
-	  System.out.println("Asking for pnfs info");
-	  _connection.sendObject(  
-	         "PnfsManager" , "info" , this , REQ_PNFSMGR_INFO
-		                      ) ;
-	              
-       }catch(Exception e ){
-          displayErrorMessage("Couldn't send query to server.\n"+e.getMessage() );	        
-       }
-   }
-   private void displayErrorMessage(String errorMessage ){
-         JOptionPane.showMessageDialog(
-               PnfsManagerPanel.this, errorMessage ,
-               "Server Problem",JOptionPane.ERROR_MESSAGE);
-   }
-   private void setWaiting( boolean waiting ){
-	   
-   }
-   public void actionPerformed( ActionEvent event ){
-   
-   }
+        add( _switchPanel , "Center" ) ;
 
-   private class Controller 
-           extends JPanel 
-	   implements ActionListener  {
-   
-      private JButton   _updateTree   = new JButton("Update") ;
-      private JButton   _displayMode  = new JButton("Toggle Mode") ;
-      private JComboBox _autoUpdate   = null ;
-      private boolean   _weAreWaiting = false ;
-      
-      private javax.swing.Timer _autoUpdateTimer  = new javax.swing.Timer( 100 , this ) ; 
-      
-      private class AutoObject {
-         private String name ;
-	 private long   interval ;
-	 private AutoObject( String name , long interval ){
-	   this.name = name ; this.interval = interval ;
-	 }
-	 public String toString(){return name ; }
-	 public long getInterval(){ return interval ; }
-      }
-      private AutoObject [] _autoLabels = {
-            new AutoObject("Manual",0L) , 
-	    new AutoObject("2 Sec",2L), 
-	    new AutoObject("10 Sec",10L), 
-	    new AutoObject("30 Sec",30L),
-	    new AutoObject("1 min" ,60L),
-     };
-    
-     private Controller(){
-      
-         setLayout( new FlowLayout(FlowLayout.CENTER,10,10) ) ; 
-         setBorder( BorderFactory.createEtchedBorder( ) ) ;
-	 
-	 _autoUpdate = new JComboBox(_autoLabels);
-	 
-	 _autoUpdate.addActionListener(this) ;
-	 _updateTree.addActionListener(this);
-	 _displayMode.addActionListener(this);
-	 
-	 add( _autoUpdate ) ;
-	 add( _updateTree ) ;
-	 add( _displayMode ) ;
-	 
- 	 
-      }
-      public void actionPerformed( ActionEvent event ){
-         Object source = event.getSource() ;
+    }
+    public void domainAnswerArrived( Object obj , int subid ){
 
-         if( source == _updateTree ){
+        if( subid == REQ_PNFSMGR_INFO ){
 
-	     askForPnfsInfo() ;
+            if( ( obj == null ) ){
+                //
+                // This is a timeout
+                //
+                //setWaiting(false);
+                displayErrorMessage("Request to PnfsManager timed out" );
 
-	 }else if( source == _autoUpdate ){
+            }else if( obj instanceof dmg.cells.nucleus.NoRouteToCellException ){
+                //
+                // The SrmSpaceManager is not reachable.
+                //
+                setWaiting(false);
+                displayErrorMessage("PnfsManager not present !" );
 
-	     AutoObject auto = (AutoObject)_autoUpdate.getSelectedItem() ;
-	     if( auto == null )return ;
-	     if( auto.interval == 0 ){
-	         _autoUpdateTimer.stop() ;
-		 _updateTree.setEnabled(true);
-             }else{
-	         _autoUpdateTimer.stop() ;
-		 _autoUpdateTimer.setDelay( (int) (auto.interval*1000L ) ) ;
-	         _autoUpdateTimer.start() ;
-		 _updateTree.setEnabled(false);
- 	     }
+            }else if( obj instanceof String ){
+                System.out.println(obj.toString());
+                try{
 
-	 }else if( source == _displayMode ){
+                    PnfsManagerInfo pnfsManagerInfo = PnfsManagerInfo.decodePnfsManagerInfo( (String)obj ) ;
 
-            _switchLayout.next( _switchPanel ) ;
-            System.out.println("Switching ...");
+                    _counterDisplay.setPnfsManagerInfo( pnfsManagerInfo );
+                    _detailPanel.setMap( pnfsManagerInfo._statisticsMap ) ;
 
-	 }else if( source == _autoUpdateTimer ){
+                }catch(PnfsManagerDecodingException pee ){
+                    pee.printStackTrace();
+                    setWaiting(false);
+                    displayErrorMessage("Problem in decoding message from PnfsManager at line "+pee.getLine() );
+                }catch(Exception ee ){
+                    ee.printStackTrace();
+                    setWaiting(false);
+                    displayErrorMessage("Problem in decoding message from PnfsManager" );
+                }
+            }
+        }
+    }
+    private void askForPnfsInfo(){
+        try{
+            System.out.println("Asking for pnfs info");
+            _connection.sendObject(
+                    "PnfsManager" , "info" , this , REQ_PNFSMGR_INFO
+            ) ;
 
-	     _updateTree.setEnabled(false);
-             System.out.println("Ping : "+System.currentTimeMillis());
-	     askForPnfsInfo() ;
+        }catch(Exception e ){
+            displayErrorMessage("Couldn't send query to server.\n"+e.getMessage() );
+        }
+    }
+    private void displayErrorMessage(String errorMessage ){
+        JOptionPane.showMessageDialog(
+                PnfsManagerPanel.this, errorMessage ,
+                "Server Problem",JOptionPane.ERROR_MESSAGE);
+    }
+    private void setWaiting( boolean waiting ){
 
-	 }
-      }
-   }
-   
+    }
+    public void actionPerformed( ActionEvent event ){
+
+    }
+
+    private class Controller
+            extends JPanel
+            implements ActionListener  {
+
+        private JButton   _updateTree   = new JButton("Update") ;
+        private JButton   _displayMode  = new JButton("Toggle Mode") ;
+        private JComboBox _autoUpdate   = null ;
+        private boolean   _weAreWaiting = false ;
+
+        private javax.swing.Timer _autoUpdateTimer  = new javax.swing.Timer( 100 , this ) ;
+
+        private class AutoObject {
+            private String name ;
+            private long   interval ;
+            private AutoObject( String name , long interval ){
+                this.name = name ; this.interval = interval ;
+            }
+            public String toString(){return name ; }
+            public long getInterval(){ return interval ; }
+        }
+        private AutoObject [] _autoLabels = {
+                new AutoObject("Manual",0L) ,
+                new AutoObject("2 Sec",2L),
+                new AutoObject("10 Sec",10L),
+                new AutoObject("30 Sec",30L),
+                new AutoObject("1 min" ,60L),
+        };
+
+        private Controller(){
+
+            setLayout( new FlowLayout(FlowLayout.CENTER,10,10) ) ;
+            setBorder( BorderFactory.createEtchedBorder( ) ) ;
+
+            _autoUpdate = new JComboBox(_autoLabels);
+
+            _autoUpdate.addActionListener(this) ;
+            _updateTree.addActionListener(this);
+            _displayMode.addActionListener(this);
+
+            add( _autoUpdate ) ;
+            add( _updateTree ) ;
+            add( _displayMode ) ;
+
+
+        }
+        public void actionPerformed( ActionEvent event ){
+            Object source = event.getSource() ;
+
+            if( source == _updateTree ){
+
+                askForPnfsInfo() ;
+
+            }else if( source == _autoUpdate ){
+
+                AutoObject auto = (AutoObject)_autoUpdate.getSelectedItem() ;
+                if( auto == null )return ;
+                if( auto.interval == 0 ){
+                    _autoUpdateTimer.stop() ;
+                    _updateTree.setEnabled(true);
+                }else{
+                    _autoUpdateTimer.stop() ;
+                    _autoUpdateTimer.setDelay( (int) (auto.interval*1000L ) ) ;
+                    _autoUpdateTimer.start() ;
+                    _updateTree.setEnabled(false);
+                }
+
+            }else if( source == _displayMode ){
+
+                _switchLayout.next( _switchPanel ) ;
+                System.out.println("Switching ...");
+
+            }else if( source == _autoUpdateTimer ){
+
+                _updateTree.setEnabled(false);
+                System.out.println("Ping : "+System.currentTimeMillis());
+                askForPnfsInfo() ;
+
+            }
+        }
+    }
+
 }
